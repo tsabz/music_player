ac0a1576fdb9ac1449965eb330fa48b2d19415fd
tsabz <tonia.saba@gmail.com>
2020-11-13 23:38:31 -0500

    imported fontawesome icons

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 const Player = () => {
     return(
@@ -8,7 +10,9 @@ const Player = () => {
                 <input type="range"/>
                 <p>End Time</p>
             </div>
-            <div className="play-control"></div>
+            <div className="play-control">
+                <FontAwesomeIcon className="play" icon={faPlay} />
+            </div>
         </div>
     )
 }

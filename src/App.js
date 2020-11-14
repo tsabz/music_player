da5eaee93be608abd8b3800598cd566afb3a0b16
tsabz <tonia.saba@gmail.com>
2020-11-13 23:27:39 -0500

    created song and player components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+// Adding Components
+import Player from './components/Player';
+import Song from './components/Song';
 
 function App() {
   
   return (
     <div className="App">
-      <h1>hello</h1>
-      
+      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
